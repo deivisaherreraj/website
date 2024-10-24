7b280c0d1a02435aa21e8adb63bc7a606857c80f
Deivis Andres Herrera Julio <deivisaherreraj@gmail.com>
2024-10-24 15:54:34 -0500

    Cambios :: Ajustes para la contrucción de la publicación remota

--- website_frontend/local_build.sh
+++ website_frontend/local_build.sh
@@ -1,25 +1,29 @@
 #! /bin/bash
 
 echo -e "Start running the script!"
-echo -e "Step 1: Upgrading pip..."
+
+echo -e "Step 1: Activating the virtual environment..."
+source .venv/bin/activate
+
+echo -e "Step 2: Upgrading pip..."
 pip install --upgrade pip
 
-echo -e "Step 2: Installing Python dependencies from requirements.txt..."
+echo -e "Step 3: Installing Python dependencies from requirements.txt..."
 pip install -r requirements.txt
 
-echo -e "Step 3: Removing existing 'public' directory..."
+echo -e "Step 4: Removing existing 'public' directory..."
 rm -rf public
 
-echo -e "Step 4: Initializing reflex..."
+echo -e "Step 5: Initializing reflex..."
 reflex init
 
-echo -e "Step 5: Exporting with reflex (frontend-only)..."
-API_URL=https://dherrerajdev-api.up.railway.app reflex export --frontend-only
+echo -e "Step 6: Exporting with reflex (frontend-only)..."
+reflex export --frontend-only
 
-echo -e "Step 6: Unzipping frontend files to 'public' directory..."
+echo -e "Step 7: Unzipping frontend files to 'public' directory..."
 unzip frontend.zip -d public
 
-echo -e "Step 7: Removing frontend.zip..."
+echo -e "Step 8: Removing frontend.zip..."
 rm -f frontend.zip
 
 echo -e "All steps executed successfully!"
